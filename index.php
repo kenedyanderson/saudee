f806d1e3719c7d481d669d83dbf9dd5364c975e7
Anderson Pereira <kenedyanderson2000@hotmail.com>
2018-02-09 18:21:19 -0300

    Inicializando Projeto Saudee

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!-- 
+  Unauthorized copying of this file, via any medium is strictly prohibited
+  Proprietary and confidential
+  Written by Anderson Pereira <andersonpereiradossantos@outlook.com>, 09/02/2018
+-->
+<html>
+    <head>
+        <title>Saudee</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- Chrome, Firefox OS and Opera -->
+        <meta name="theme-color" content="#14ada1">
+        <!-- Windows Phone -->
+        <meta name="msapplication-navbutton-color" content="#14ada1">
+        <!-- iOS Safari -->
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <meta name="viewport" content="width=device-width, user-scalable=no">
+        <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+        <link rel="icon" href="images/favicon.png" type="image/x-icon">   
+        <link rel="shortcut icon" href="" type="image/x-icon">
+        <link rel="icon" href="" type="image/x-icon">
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
+        <link rel="stylesheet" href="styles/font-awesome.min.css">
+        <link rel="stylesheet" href="styles/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <script type="text/javascript" src="scripts/script.js"></script>
+        <script src="scripts/jquery.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <span class="border ">Anderson</span>
+            <span class="border-top"></span>
+            <span class="border-right"></span>
+            <span class="border-bottom"></span>
+            <span class="border-left"></span>
+        </div>
+    </body>
+</html>
